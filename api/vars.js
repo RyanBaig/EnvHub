2f4c8ceca197ff02ba4a5faaf33e6f524d76b3b3
RyanGamingYT <rg373429@gmail.com>
2023-11-26 21:16:57 +0500

    set config

--- api/vars.js
+++ api/vars.js
@@ -1,14 +1,17 @@
-// Set Appwrite configurations
-// client
-//   .setEndpoint("https://cloud.appwrite.io/v1")
-//   .setProject("envhub")
-//   .setKey(
-//     "658ee3ae6bf33c40348e1068bf0ee92773b07eb5f2b53152232e1434ea563b5b0d2bb799f911db728e51f2557e7efb16b9f971a2ae9df90f3a2f042e746203aa8c6776b6420c4f91315a95bfa565ebc13c56d283cefa51a11882d09cbcd1849f532b8a5cdebebc35fec980baa9c1a2ed33a0d5c82cbf68b40539b2a6b9b457a4"
-//   );
+
+ 
 const sdk = require("node-appwrite");
 
 // Initialize Appwrite client
 const client = new sdk.Client();
+
+// Set Appwrite configurations
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("envhub")
+  .setKey(
+    "658ee3ae6bf33c40348e1068bf0ee92773b07eb5f2b53152232e1434ea563b5b0d2bb799f911db728e51f2557e7efb16b9f971a2ae9df90f3a2f042e746203aa8c6776b6420c4f91315a95bfa565ebc13c56d283cefa51a11882d09cbcd1849f532b8a5cdebebc35fec980baa9c1a2ed33a0d5c82cbf68b40539b2a6b9b457a4"
+  );
 
 // Initialize the Databases service
 const databases = new sdk.Databases(client);
