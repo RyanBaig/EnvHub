b1b6289d3ee1767e464b734449b2f6408f6c4d66
RyanGamingYT <rg373429@gmail.com>
2023-11-26 15:50:23 +0500

    experimenting

--- api/vars.js
+++ api/vars.js
@@ -1,48 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const token = process.env.VERCEL_AUTH_TOKEN_FOR_ENV_HUB;
+if (!token) {
+    const token = "QUhi52tmDlQBhj39M0Ug3tLo";
 
-// function to fetch the vars from Vercel API
-const fetchVarsFromVercel = async () => {
-  // Make a request to the Vercel API to get the environment vars
-  const response = await fetch(
-    "https://api.vercel.com/v1/projects/EnvHub/env",
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
-  const data = await response.json();
+    // function to fetch the vars from Vercel API
+    const fetchVarsFromVercel = async () => {
+        // Make a request to the Vercel API to get the environment vars
+        const response = await fetch(
+            "https://api.vercel.com/v1/projects/EnvHub/env",
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        const data = await response.json();
 
-  // Put all the environment vars in an iterable
-  const environmentVars = Object.entries(data).map(([key, value]) => ({
-    key,
-    value,
-  }));
+        // Put all the environment vars in an iterable
+        const environmentVars = Object.entries(data).map(([key, value]) => ({
+            key,
+            value,
+        }));
 
-  return environmentVars;
-};
+        return environmentVars;
+    };
 
-router.get('/:varName', async (req, res) => {
-  const varName = req.params.varName;
-  console.log("YAHOOO")
-  if (varName === "VERCEL_AUTH_TOKEN_FOR_ENV_HUB") {
-    res.status(404).send({ msg: "Variable not found", status: 404 });
-    return;
-  }
+    router.get('/:varName', async (req, res) => {
+        const varName = req.params.varName;
+        console.log("YAHOOO")
+        if (varName === "VERCEL_AUTH_TOKEN_FOR_ENV_HUB") {
+            res.status(404).send({ msg: "Variable not found", status: 404 });
+            return;
+        }
   
-  try {
-    const varsFromVercel = await fetchVarsFromVercel();
-    const matchedVar = varsFromVercel.find((envVar) => envVar.key === varName);
+        try {
+            const varsFromVercel = await fetchVarsFromVercel();
+            const matchedVar = varsFromVercel.find((envVar) => envVar.key === varName);
 
-    if (matchedVar) {
-      res.send({ value: matchedVar.value });
-    } else {
-      res.status(404).send({ msg: 'Variable not found', status: 404 });
-    }
-  } catch (error) {
-    console.error('Error retrieving vars:', error);
-    res.status(500).send({ msg: 'Error Retrieving vars for Database', status: 500 });
-  }
-});
+            if (matchedVar) {
+                res.send({ value: matchedVar.value });
+            } else {
+                res.status(404).send({ msg: 'Variable not found', status: 404 });
+            }
+        } catch (error) {
+            console.error('Error retrieving vars:', error);
+            res.status(500).send({ msg: 'Error Retrieving vars for Database', status: 500 });
+        }
+    });
+};
