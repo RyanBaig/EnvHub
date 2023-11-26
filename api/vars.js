0a7dffb686568795178c7ef2eae19e67d24eb3e5
RyanGamingYT <rg373429@gmail.com>
2023-11-26 21:04:15 +0500

    hello world

--- api/vars.js
+++ api/vars.js
@@ -1,3 +1,10 @@
+// Set Appwrite configurations
+// client
+//   .setEndpoint("https://cloud.appwrite.io/v1")
+//   .setProject("envhub")
+//   .setKey(
+//     "658ee3ae6bf33c40348e1068bf0ee92773b07eb5f2b53152232e1434ea563b5b0d2bb799f911db728e51f2557e7efb16b9f971a2ae9df90f3a2f042e746203aa8c6776b6420c4f91315a95bfa565ebc13c56d283cefa51a11882d09cbcd1849f532b8a5cdebebc35fec980baa9c1a2ed33a0d5c82cbf68b40539b2a6b9b457a4"
+//   );
 const sdk = require("node-appwrite");
 
 // Initialize Appwrite client
@@ -10,37 +17,35 @@ const databases = new sdk.Databases(client);
 const databaseId = "env_vars";
 const collectionId = "key-value-pair";
 
-// Set Appwrite configurations
-client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("envhub")
-  .setKey(
-    "658ee3ae6bf33c40348e1068bf0ee92773b07eb5f2b53152232e1434ea563b5b0d2bb799f911db728e51f2557e7efb16b9f971a2ae9df90f3a2f042e746203aa8c6776b6420c4f91315a95bfa565ebc13c56d283cefa51a11882d09cbcd1849f532b8a5cdebebc35fec980baa9c1a2ed33a0d5c82cbf68b40539b2a6b9b457a4"
-  );
-
 // Serverless function handler
-const handler = (req, res) => {
-  const varName = req.query.varName; // Extract varName from URL
+const handler = async (req, res) => {
+  try {
+    const varName = req.query.varName; // Extract varName from URL
 
-  // List documents to find a document with ID matching varName
-  databases
-    .listDocuments(databaseId, collectionId, [varName])
-    .then((response) => {
+    // Create a query to filter by document ID
+    const query = new sdk.Query();
+    query.setFilters([`$id=${varName}`]);
+
+    // List documents with the specified query
+    const response = await databases.listDocuments(databaseId, collectionId, [
+      query,
+    ]);
+
+    // Extract the value from the first document (assuming there's only one match)
+    if (response.documents.length > 0) {
       const document = response.documents[0];
-
-      if (document) {
-        const value = document.fields.value;
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ varName, value }));
-      } else {
-        res.writeHead(404, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Variable not found" }));
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.writeHead(500, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "Internal Server Error" }));
-    });
+      const value = document.fields.value;
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ varName, value }));
+    } else {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "Variable not found" }));
+    }
+  } catch (error) {
+    console.error(error);
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Internal Server Error" }));
+  }
 };
+
 module.exports = handler;
